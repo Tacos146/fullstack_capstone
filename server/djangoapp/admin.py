ba07979412cca887541eb8c5aacd9989206965b4
Tacos146 <takkiy2@gmail.com>
2025-01-30 10:05:50 -0500

    module3-2

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,9 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Registering models with their respective admins
+# admin.site.register(CarMake)
+# admin.site.register(CarModel)
 
 
 # Register your models here.
@@ -11,3 +15,15 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+# CarMake を管理サイトに登録
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # 一覧ページで表示する項目
+
+# CarModel を管理サイトに登録
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')  # 一覧ページで表示する項目
+    list_filter = ('car_make', 'type', 'year')  # フィルタリング機能を追加
+    search_fields = ('name',)  # 検索バーで検索可能にする
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
